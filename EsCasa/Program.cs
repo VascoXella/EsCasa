c2be40780d4410e9b82081b250440de45dd73072
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-06 11:26:29 +0100

    Aggiungere i file di progetto.

---
+++ EsCasa/Program.cs
@@ -0,0 +1,31 @@
+﻿using EsercizioCasa.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioCasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2, 2);
+
+            Vector v2 = new Vector(3, 3);
+
+            Vector v3 = new Vector();
+            v3 = Vector.Parser("4; 4");
+
+
+            Console.WriteLine("{0}, {1}", v1.Sommav(v2).x, v3.Sommav(v2).y);
+
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
---
+++ EsCasa/Properties/Vector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioCasa.Properties
+{
+    internal class Vector
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+
+        public Vector()
+        {
+        }
+
+        public Vector(float X, float Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+
+        public static Vector Parser(string a)
+        {
+            Vector V = new Vector();
+
+            a.Split();
+            string[] ss = a.Split(';');
+            foreach (string k in ss)
+            {
+                V.x = (float)float.Parse(Console.ReadLine());
+                V.y = (float)float.Parse(Console.ReadLine());
+            }
+            return V;
+        }
+
+        public Vector Sommav(Vector v2)
+        {
+            Vector V = new Vector();
+            V.x = this.x + v2.x;
+            V.y = this.y + v2.y;
+            return V;
+        }
+
+    }
+}
